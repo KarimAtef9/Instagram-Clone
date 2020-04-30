a11a40c478596d409f81e8df2f4e8e317ead8199
Karim <karimatef65@gmail.com>
2020-04-30 21:03:16 +0200

    connecting users

--- app/src/main/java/com/example/instagram/Fragment/SearchFragment.java
+++ app/src/main/java/com/example/instagram/Fragment/SearchFragment.java
@@ -1,23 +1,150 @@
 package com.example.instagram.Fragment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.example.instagram.Model.User;
+import com.example.instagram.Model.UserAdapter;
 import com.example.instagram.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
 
 
 public class SearchFragment extends Fragment {
+    private ListView usersListView;
+    private UserAdapter userAdapter;
+    private ArrayList<User> users;
+    private EditText search_edittext;
+    private Context mContext;
+
+    private FirebaseUser firebaseUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        usersListView = view.findViewById(R.id.search_listView);
+        search_edittext = view.findViewById(R.id.search_editText);
+
+        users = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext(), users);
+        usersListView.setAdapter(userAdapter);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        readAllUsers();
+
+        search_edittext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if (charSequence.toString().equals("")) {
+//                    users.clear();
+//                    userAdapter.notifyDataSetChanged();
+//                } else {
+//                    searchForUsers(charSequence.toString().toLowerCase().trim());
+//                }
+                // TODO for debug purpose but after that remove all users seach option
+                searchForUsers(charSequence.toString().toLowerCase().trim());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return view;
+    }
+
+    /*
+        search for current input username
+        adapter and user list updated here
+     */
+    private void searchForUsers(String username) {
+        Query query = FirebaseDatabase.getInstance().getReference("Users").
+                orderByChild("username").startAt(username).endAt(username + "\uf8ff");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                users.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    User user = snapshot.getValue(User.class);
+                    // to prevent showing current user in search
+//                    if (! user.getId().equals(firebaseUser.getUid())) {
+//                        users.add(user);
+//                    }
+                    users.add(user);
+                }
+                userAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void readAllUsers() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // if empty search get all users
+                if (search_edittext.getText().toString().equals("")) {
+                    users.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        User user = snapshot.getValue(User.class);
+                        // to prevent showing current user in search
+//                    if (! user.getId().equals(firebaseUser.getUid())) {
+//                        users.add(user);
+//                    }
+                        users.add(user);
+                    }
+                    userAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/instagram/Model/UserAdapter.java
@@ -0,0 +1,137 @@
+package com.example.instagram.Model;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.instagram.Fragment.ProfileFragment;
+import com.example.instagram.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserAdapter extends ArrayAdapter<User> {
+    private ArrayList<User> userList;
+    // required for getting image
+    private Context mContext;
+
+    private FirebaseUser firebaseUser;
+
+
+    public UserAdapter(Context context, ArrayList<User> userList) {
+        super(context, 0, userList);
+        this.mContext = context;
+        this.userList = userList;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.user_item, parent, false);
+        }
+
+        TextView username = listItemView.findViewById(R.id.username_textview);
+        TextView fullName = listItemView.findViewById(R.id.fullname_textview);
+        CircleImageView profileImage = listItemView.findViewById(R.id.profile_image);
+        final Button followBtn = listItemView.findViewById(R.id.follow_btn);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        final User user = userList.get(position);
+
+        followBtn.setVisibility(View.VISIBLE);
+        username.setText(user.getUsername());
+        fullName.setText(user.getFullName());
+        Glide.with(mContext).load(user.getImageUrl()).into(profileImage);
+
+        // setting text on follow button
+        setFollowButtonText(user.getId(), followBtn);
+
+        if (user.getId().equals(firebaseUser.getUid())) {
+            followBtn.setVisibility(View.GONE);
+        }
+
+        // to open clicked user profile
+        listItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = mContext.getSharedPreferences("PREFS", Context.MODE_PRIVATE).edit();
+                editor.putString("profileId", user.getId());
+                editor.apply();
+
+                ((FragmentActivity)mContext).getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new ProfileFragment()).commit();
+            }
+        });
+
+        followBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // not friends
+                if (followBtn.getText().toString().equals("Follow")) {
+                    FirebaseDatabase.getInstance().getReference().child("Follow")
+                            .child(firebaseUser.getUid()).child("Following")
+                            .child(user.getId()).setValue(true);
+
+                    FirebaseDatabase.getInstance().getReference().child("Follow")
+                            .child(user.getId()).child("Followers")
+                            .child(firebaseUser.getUid()).setValue(true);
+                } else {
+                    // already friends, then un-friend
+                    FirebaseDatabase.getInstance().getReference().child("Follow")
+                            .child(firebaseUser.getUid()).child("Following")
+                            .child(user.getId()).removeValue();
+
+                    FirebaseDatabase.getInstance().getReference().child("Follow")
+                            .child(user.getId()).child("Followers")
+                            .child(firebaseUser.getUid()).removeValue();
+                }
+            }
+        });
+
+
+        return listItemView;
+    }
+
+
+    // setting text on button (follow or following)
+    private void setFollowButtonText(final String userId, final Button followBtn) {
+        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference()
+                .child("Follow").child(firebaseUser.getUid()).child("Following");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(userId).exists()) {
+                    followBtn.setText("Following");
+                } else {
+                    followBtn.setText("Follow");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+}
